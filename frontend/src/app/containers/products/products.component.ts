517bc11344dcb5494cb423c65b58e4336ed82180
Alexandur Tir <alex.tir.92@gmail.com>
2020-04-13 19:51:20 +0200

    add images to product

--- frontend/src/app/containers/products/products.component.ts
+++ frontend/src/app/containers/products/products.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { ProductService } from 'src/app/services/product.service';
+import { CategoryService } from 'src/app/services/category.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+  public products;
+  public categories;
+  public product = { id: 0, name: '', reference: '', price: 0, image: '', CategoryId: '' }
+  public message: string;
+  public validateForm: FormGroup;
 
-  constructor() { }
+  constructor(public productService: ProductService,
+    public categoryService: CategoryService,
+    private router: Router,
+    private fb: FormBuilder,
+    private notification: NzNotificationService) { }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+      this.getAll();
   }
-
+  getAll() {
+    this.productService.getAll()
+      .subscribe(res => { this.products = res; },
+        error => console.error(error));
+  }
 }
